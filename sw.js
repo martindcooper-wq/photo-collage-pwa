d8224113cace70fe4d5cb596db6ea133d92d9531
martindcooper-wq <martind_cooper@hotmail.co.uk>
2026-02-19 22:45:33 +0000

    Add service worker configuration for Photo Collage

---
+++ sw.js
@@ -0,0 +1,9 @@
+{
+  "name": "Photo Collage",
+  "short_name": "Collage",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#111111",
+  "icons": []
+}
